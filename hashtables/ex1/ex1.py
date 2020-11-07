f07e6cf8d0225de2dbc49b223f8d610b175019a7
aland1013 <adam.land@gmail.com>
2020-11-06 21:15:18 -0500

    implemented ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,22 @@
 def get_indices_of_item_weights(weights, length, limit):
     """
-    YOUR CODE HERE
+    create an empty hash table
+    for each weight in weights:
+        - calculate the target by subtracting weight from limit
+        - if the target is stored in the hash table, return the 
+          current index and the target index
+        - else, store the index and weight in the hash table
     """
-    # Your code here
-
+    ht = {}
+    
+    for i in range(length):
+        target = limit - weights[i]
+        
+        if target in ht.values():
+            x = i
+            y = [k for k,v in ht.items() if v == target][0]
+            return (x, y) if x > y else (y, x)
+        
+        ht[i] = weights[i]
+        
     return None
